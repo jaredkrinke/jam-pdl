103d311d64381b251837258401e11cfe3da958e2
jaredkrinke <jaredkrinke@gmail.com>
2014-03-09 21:45:27 -0700

    Laying out basic data structures

--- js/main.js
+++ js/main.js
@@ -1,6 +1,58 @@
 ﻿/// <reference path="radius.js" />
 /// <reference path="radius-ui.js" />
 
+// N-dimensional array
+function NArray(dimensions) {
+    // TODO
+}
+
+// Arguments: value, x1, x2, x3, etc.
+NArray.prototype.set = function () {
+    // TODO
+};
+
+// Arguments: x1, x2, x3, etc.
+NArray.prototype.get = function () {
+    // TODO
+};
+
+// Arguments: x1, x2, x3, etc.
+NArray.prototype.remove = function () {
+    // TODO
+};
+
+NArray.prototype.clear = function () {
+    // TODO
+};
+
+function VectorSet(dimensions) {
+    // TODO
+}
+
+VectorSet.prototype.add = function () {
+    // TODO
+};
+
+VectorSet.prototype.removeIndex = function (index) {
+    // TODO
+};
+
+VectorSet.prototype.removeRandom = function (index) {
+    // TODO
+};
+
+VectorSet.prototype.contains = function (index) {
+    // TODO
+};
+
+VectorSet.prototype.getCount = function (index) {
+    // TODO
+};
+
+VectorSet.prototype.clear = function (index) {
+    // TODO
+};
+
 function GameLayer() {
     Layer.call(this);
 }
