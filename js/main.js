f2cd26a33f36195ed29c74609bd218a86362eb72
jaredkrinke <jaredkrinke@gmail.com>
2014-03-12 07:37:22 -0700

    Initial work on world

--- js/main.js
+++ js/main.js
@@ -96,6 +96,88 @@ VectorSet.prototype.clear = function () {
     this.list.length = 0;
 };
 
+function World() {
+    Entity.call(this);
+
+    // Bounds (inclusive, note that there is no limit to the right)
+    this.x1 = 0;
+    this.y1 = -4;
+    this.y2 = 4;
+
+    var minColumns = 32;
+    this.lastColumn = 0;
+
+    // World contents
+    this.walls = new Vector(3);
+    this.squares = new Vector(2);
+
+    // World state
+    this.squaresUsed = new VectorSet(2);
+    this.wallsAvailable = new VectorSet(3);
+    this.wallsConsidered = new VectorSet(3);
+
+    // Events
+    this.changed = new Event();
+
+    // Constants
+    var wallOffsets = [
+        [[0, 0], [0, 1]],
+        [[0, 0], [1, 0]],
+    ];
+}
+
+World.prototype = Object.create(Entity.prototype);
+World.offsetWalls = [[-1, 0], [0, 0], [0, 0], [0, -1]];
+World.wallAxes = [2, 2, 1, 1];
+
+World.prototype.reset = function () {
+    this.lastColumn = 0;
+    this.walls.clear();
+    this.squares.clear();
+
+    this.squaresUsed.clear();
+    this.wallsAvailable.clear();
+    this.wallsConsidered.clear();
+
+    this.annexSquare(0, 0);
+    this.ensureColumnsComplete(0);
+};
+
+World.prototype.forEachSquareWall = function (x, y, f) {
+    var count = World.offsetWalls.length;
+    for (var i = 0; i < count; i++) {
+        var offset = World.offsetWalls[i];
+        var wallX = x + offset[0];
+        var wallY = y + offset[1];
+
+        f.call(this, World.wallAxes[i], wallX, wallY, i);
+    }
+};
+
+World.prototype.ensureWalls = function (x, y) {
+    this.forEachSquareWall(x, y, function (axis, x, y, direction) {
+        if (!this.walls.get(x, y)) {
+            this.walls.set(true, axis, x, y);
+        }
+    });
+};
+
+World.prototype.ensureSquare = function (x, y) {
+    if (!this.squares.get(x, y)) {
+        this.squares.set(true, x, y);
+        this.ensureWalls(x, y);
+    }
+};
+
+// TODO: checkWallRemovable
+// TODO: annexSquare
+// TODO: removeWall
+// TODO: carve
+// TODO: ensureWallConsidered
+// TODO: ensureColumnComplete
+// TODO: ensureColumnsComplete
+// TODO: checkMove
+
 function GameLayer() {
     Layer.call(this);
 }
